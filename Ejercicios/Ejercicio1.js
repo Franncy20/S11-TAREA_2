9824982679ff693fd0067e6442d3956d0f3447a4
Franncy20 <flucerod@unemi.edu.ec>
2025-11-14 15:25:15 -0500

    Ejercicio1.js

--- Ejercicios/Ejercicio1.js
+++ Ejercicios/Ejercicio1.js
@@ -2,8 +2,8 @@
 //Solicita el número de versión actual y la nueva versión propuesta. Si la nueva versión es mayor,
 //muestra “Actualización disponible”; si es igual, “Versión actualizada”.
 
-let vActual = "1.0.0"
-let vPropuesta = "1.0.0"
+let vActual = 1.0
+let vPropuesta = 1.0
 
 if (vPropuesta > vActual) {
     console.log("Actualización disponible")
@@ -11,4 +11,5 @@ if (vPropuesta > vActual) {
     console.log("Versión actualizada")
 } else {
     console.log("Actualización no disponible")
+
 }
